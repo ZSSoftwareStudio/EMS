c2d81bad10baca01f5486b37c27efcdfe36a8deb
Zarifprogrammer <Zarifprogrammer@gmail.com>
2020-12-20 15:20:48 +0600

    Sign out function created

--- dashboard/_checklogin.php
+++ dashboard/_checklogin.php
@@ -1,10 +1,10 @@
 <?php
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $email = $_POST['email'];
-    
     setcookie("admin", $email, time() + (86400 * 365), '/');
     header("location: dashboard.php");
   }else{
-      header("location: login.php");
+    setcookie("admin", $email, time() - (86400 * 365), '/');
+    header("location: login.php");
   }
 ?>
