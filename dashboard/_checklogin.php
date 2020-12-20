1cc27161c1d6ba2fcb6da94613e421ce604a9858
Zarifprogrammer <Zarifprogrammer@gmail.com>
2020-12-20 10:17:02 +0600

    Dashboard Updated

--- dashboard/_checklogin.php
+++ dashboard/_checklogin.php
@@ -1,6 +1,9 @@
 <?php
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
-      header("location: dashboard.php");
+    $email = $_POST['email'];
+    
+    setcookie("admin", $email, time() + (86400 * 365), '/');
+    header("location: dashboard.php");
   }else{
       header("location: login.php");
   }
